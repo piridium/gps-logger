ea8784d162e23aed34ebae102b9f1a5a43119087
Patrick Itten <info@patrickitten.ch>
2020-07-07 08:39:31 +0200

    seems to be working. a bit low on memory

--- gps-logger/gps-logger.ino
+++ gps-logger/gps-logger.ino
@@ -5,6 +5,9 @@
 #include <SPI.h>
 #include <SD.h>
 
+#define LOG_FREQ 10000
+#define UPD_FREQ 5000
+
 // Display
 // 0X3C+SA0 - 0x3C or 0x3D
 #define DISP_ADDRESS 0x3C
@@ -13,60 +16,61 @@
 SSD1306AsciiAvrI2c oled;
 
 // GPS
-static const int RXPin = 2, TXPin = 3;
-static const uint32_t GPSBaud = 9600;
+#define GPSBaud 9600
 // The TinyGPS++ object
 TinyGPSPlus gps;
 // The serial connection to the GPS device
-SoftwareSerial ss(RXPin, TXPin);
+SoftwareSerial ss(2, 3); // (RX,TX)
 
 // SD
 // set up variables using the SD utility library functions:
-Sd2Card card;
-SdVolume volume;
-SdFile root;
-const int chipSelect = 4;
+// Sd2Card card;
+// SdVolume volume;
+// SdFile root;
+#define chipSelect 4
 bool sdFailure = false;
+unsigned long logTime;
 
 // Buffer
 // global buffer for datapoints
-char buffer[64];
+char buffer[42]; // longest buffer seems to be: "<trkpt lat="047.575336" lon="009.368504">" : 41 (+1 for NULL terminator)
+// int bufferSize = 0;
+
+// GPX
+// #define GPX_HEADER ""
 
 void setup()
 {
   // DISPLAY
-  #if RST_PIN >= 0
+  if (RST_PIN >= 0){
     oled.begin(&Adafruit128x64, DISP_ADDRESS, RST_PIN);
-  #else // RST_PIN >= 0
+  } else {
     oled.begin(&Adafruit128x64, DISP_ADDRESS);
-  #endif // RST_PIN >= 0
-    // Call oled.setI2cClock(frequency) to change from the default frequency.
+  } // Call oled.setI2cClock(frequency) to change from the default frequency.
   oled.setFont(font5x7);
   oled.clear();
-  oled.println("GPS LOGGER");
+  oled.println(F("GPS LOGGER"));
   oled.println(F("(c)2020 Patrick Itten"));
-  oled.println("initializing...");
+  oled.println();
+  oled.println(F("initializing..."));
 
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  // SERIAL MONITOR
+  Serial.begin(115200);
+  while (!Serial);  // wait for serial port to connect. Needed for native USB
   Serial.println(F("GPS-LOGGER"));
-  Serial.print(F("TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
   Serial.println(F("(c)2020 by Patrick Itten"));
   Serial.println();
 
   // SD
-  Serial.println("Initializing SD card...");
+  Serial.println(F("Initializing SD card..."));
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-//    oled.clear();
-    oled.println("...no card found.");
+    Serial.println(F("Card failed, or not present"));
+    oled.println(F("...no card found."));
     sdFailure = true;
   } else {
-    Serial.println("card initialized.");
-    oled.println("...card initialized.");
+    Serial.println(F("card initialized."));
+    oled.println(F("...card initialized."));
   }
   Serial.println();
   delay(3000);
@@ -82,70 +86,22 @@ void loop()
 
   if (gps.location.isUpdated()){
     showInfo(true); // true: new position
-    writeLog();
+    if (millis() - logTime > LOG_FREQ ){
+      writeLog();
+      logTime = millis();
+    }
   } else {
     showInfo(false); // false: same position
   }
-  
-  smartDelay(1000); // Log frequency
+  smartDelay( UPD_FREQ ); // Update frequency
 
   if (millis() > 5000 && gps.charsProcessed() < 10)
     Serial.println(F("No GPS data received: check wiring"));
 }
 
-void writeLog(){
-  char filename[16];
-  sprintf(filename, "%02d%02d%02d.gpx", gps.date.year(), gps.date.month(), gps.date.day());
-  File dataFile = SD.open(filename, FILE_WRITE);
-  if (dataFile) {
-    // char datarow[128];
-    // sprintf(datarow, "<trkpt lat=\"%.6f\" lon=\"%.6f\"></trkpt>", gps.location.lat(), gps.location.lng());
-//    dataFile.println(buffer);
-    char buf[32];
-    // opening
-    strcpy (buffer, "<trkpt lat=\"");
-    dtostrf (gps.location.lat(), 0, 6, buf);
-    strcat (buffer, buf);
-    strcat (buffer, "\" lon=\"");
-    dtostrf (gps.location.lng(), 0, 6, buf);
-    strcat (buffer, buf);
-    strcat (buffer, "\">");
-    dataFile.println(buffer);
-    // time
-    strcpy (buffer, "<time>");
-    sprintf(buf, "%02d-%02d-%02dT%02d:%02d:%02dZ", gps.date.year(), gps.date.month(), gps.date.day(), gps.time.hour(), gps.time.minute(), gps.time.second());
-    strcat (buffer, buf);
-    strcat (buffer, "</time>");
-    dataFile.println(buffer);
-    // elevation
-    gpxBuffer("ele", gps.altitude.meters(), 0);
-    dataFile.println(buffer);
-    // course
-    gpxBuffer("course", gps.course.deg(), 0);
-    dataFile.println(buffer);
-    // speed
-    gpxBuffer("speed", gps.speed.mps(), 1);
-    dataFile.println(buffer);
-    // sat
-    gpxBuffer("sat", gps.satellites.value(), 0);
-    dataFile.println(buffer);
-    // hdop
-    gpxBuffer("hdop", gps.hdop.hdop(), 1);
-    dataFile.println(buffer);
-    // closing
-    strcpy (buffer, "</trkpt>");
-    dataFile.println(buffer);
-
-    dataFile.close();
-    sdFailure = false;
-  }
-  else {
-    Serial.println("error opening logfile");
-    sdFailure = true;
-  }
-}
-
 void showInfo(bool updated){
+
+  Serial.println();
 
   if (updated){
     oled.clear();
@@ -206,6 +162,7 @@ void showInfo(bool updated){
     strcat (buffer, " ");
     Serial.print(buffer);
     oled.print(buffer);
+    //printMaxBufferSize();
     // Serial.print(gps.course.isValid() ? TinyGPSPlus::cardinal(gps.course.deg()) : "** ");
 
     // Fix age
@@ -219,12 +176,66 @@ void showInfo(bool updated){
   if (sdFailure){
     oled.print("   *SDerr");
   }
-  Serial.println();
 }
 
-// This custom version of delay() ensures that the gps object is being "fed".
+void writeLog(){
+  char filename[16];
+  sprintf(filename, "%02d%02d%02d.gpx", gps.date.year(), gps.date.month(), gps.date.day());
+  File dataFile = SD.open(filename, FILE_WRITE);
+  if (dataFile) {
+    // char datarow[128];
+    // sprintf(datarow, "<trkpt lat=\"%.6f\" lon=\"%.6f\"></trkpt>", gps.location.lat(), gps.location.lng());
+//    dataFile.println(buffer);
+    char buf[32];
+    // opening
+    strcpy (buffer, "<trkpt lat=\"");
+    dtostrf (gps.location.lat(), 0, 6, buf);
+    strcat (buffer, buf);
+    strcat (buffer, "\" lon=\"");
+    dtostrf (gps.location.lng(), 0, 6, buf);
+    strcat (buffer, buf);
+    strcat (buffer, "\">");
+    dataFile.println(buffer);
+    //printMaxBufferSize();
+    
+    // time
+    strcpy (buffer, "<time>");
+    sprintf(buf, "%02d-%02d-%02dT%02d:%02d:%02dZ", gps.date.year(), gps.date.month(), gps.date.day(), gps.time.hour(), gps.time.minute(), gps.time.second());
+    strcat (buffer, buf);
+    strcat (buffer, "</time>");
+    dataFile.println(buffer);
+    //printMaxBufferSize();
+    // elevation
+    gpxBuffer("ele", gps.altitude.meters(), 0);
+    dataFile.println(buffer);
+    // course
+    gpxBuffer("course", gps.course.deg(), 0);
+    dataFile.println(buffer);
+    // speed
+    gpxBuffer("speed", gps.speed.mps(), 1);
+    dataFile.println(buffer);
+    // sat
+    gpxBuffer("sat", gps.satellites.value(), 0);
+    dataFile.println(buffer);
+    // hdop
+    gpxBuffer("hdop", gps.hdop.hdop(), 1);
+    dataFile.println(buffer);
+    // closing
+    dataFile.println("</trkpt>");
+
+    dataFile.close();
+    sdFailure = false;
+    Serial.print("log written");
+  }
+  else {
+    Serial.println(F("error opening logfile"));
+    sdFailure = true;
+  }
+}
+
 static void smartDelay(unsigned long ms)
 {
+  // This custom version of delay() ensures that the gps object is being "fed".
   unsigned long start = millis();
   do 
   {
@@ -248,6 +259,7 @@ static void loadBuffer(float val, bool valid, char prefix[], char suffix[], int 
   }
 
   strcat (buffer, " ");
+  //printMaxBufferSize();
   smartDelay(0);
 }
 
@@ -266,7 +278,8 @@ static void gpxBuffer(char tag[], float val, int prec)
   strcat (buffer, "</");
   strcat (buffer, tag);
   strcat (buffer, ">");
-
+  
+  // printMaxBufferSize();
 }
 
 static void loadBufferDate(TinyGPSDate &d, char prefix[])
@@ -282,6 +295,7 @@ static void loadBufferDate(TinyGPSDate &d, char prefix[])
     sprintf(sz, "%02d-%02d-%02d ", d.month(), d.day(), d.year());
     strcat(buffer, sz);
   }
+  //printMaxBufferSize();
 }
 
 static void loadBufferTime(TinyGPSTime &t, char prefix[])
@@ -300,3 +314,16 @@ static void loadBufferTime(TinyGPSTime &t, char prefix[])
   // printInt(d.age(), d.isValid(), 5);
   smartDelay(0);
 }
+
+// static void printMaxBufferSize(){
+//   if (strlen(buffer) > bufferSize){
+//     Serial.println(F("********************"));
+//     Serial.print("length of \"");
+//     Serial.print(buffer);
+//     Serial.print("\" : ");
+//     Serial.println(strlen(buffer));
+//     Serial.println(F("********************"));
+
+//     bufferSize = strlen(buffer);
+//   }
+// }
---
+++ gpslogger-markfickett/gpslogger-markfickett.ino
@@ -0,0 +1,359 @@
+/**
+ * Fetch GPS data over serial and log it to an SD card. See README for more.
+ */
+
+#include <SoftwareSerial.h>
+#include <SdFat.h>
+#include <TinyGPS.h>
+
+#define SAMPLE_INTERVAL_MS 1000
+
+#define PIN_STATUS_LED 13
+
+#define PIN_RX_FROM_GPS 2
+#define PIN_TX_TO_GPS 3
+#define PIN_SD_CHIP_SELECT 4
+#define PIN_SPI_CHIP_SELECT_REQUIRED 4
+// The SD library requires, for SPI communication:
+// 11 MOSI (master/Arduino output, slave/SD input)
+// 12 MISO (master input, slave output)
+// 13 CLK (clock)
+
+#define DEFAULT_GPS_BAUD 9600
+#define GPS_BAUD 9600
+
+// Seek to fileSize + this position before writing track points.
+#define SEEK_TRKPT_BACKWARDS -24
+#define GPX_EPILOGUE "\t</trkseg></trk>\n</gpx>\n"
+#define LATLON_PREC 6
+
+#define PIN_BATTERY_DIVIDED_VOLTAGE A3
+#define VOLTAGE_ANALOG_CONSTANT 53.71
+#define CUTOFF_DV -0.1
+#define CUTOFF_VOLTAGE 3.4
+
+TinyGPS gps;
+SoftwareSerial nss(PIN_RX_FROM_GPS, PIN_TX_TO_GPS);
+
+SdFat sd;
+SdFile gpxFile;
+SdFile voltageFile;
+
+// General-purpose text buffer used in formatting.
+char buf[32];
+
+struct GpsSample {
+  float lat_deg,
+        lon_deg;
+  float altitude_m;
+
+  int satellites;
+  int hdop_hundredths;
+
+  // How many ms, according to millis(), since the last position data was read
+  // from the GPS.
+  unsigned long fix_age_ms;
+
+  float speed_mps;
+  float course_deg;
+
+  // How many ms, according to millis(), since the last datetime data was read
+  // from the GPS.
+  unsigned long datetime_fix_age_ms;
+
+  int year;
+  byte month,
+       day,
+       hour,
+       minute,
+       second,
+       hundredths;
+};
+// The latest sample read from the GPS.
+struct GpsSample sample;
+
+// The previously sampled / recorded voltages.
+float lastVoltage;
+float lastRecordedVoltage;
+byte lastRecordedVoltageMinute;
+
+void setup() {
+  pinMode(PIN_STATUS_LED, OUTPUT);
+  digitalWrite(PIN_STATUS_LED, HIGH);
+  lastVoltage = 0;
+  lastRecordedVoltage = 0;
+  lastRecordedVoltageMinute = 61;
+  Serial.begin(115200);
+  setUpSd();
+  resetGpsConfig();
+  getFirstGpsSample();
+  startFilesOnSdNoSync();
+  digitalWrite(PIN_STATUS_LED, LOW);
+}
+
+void loop() {
+  readFromGpsUntilSampleTime();
+  fillGpsSample(gps);
+  if (sample.fix_age_ms <= SAMPLE_INTERVAL_MS) {
+    // TODO: Write whenever there is new data (trust GPS is set at 1Hz).
+    writeGpxSampleToSd();
+  }
+  recordVoltage();
+}
+
+void setUpSd() {
+  if (PIN_SD_CHIP_SELECT != PIN_SPI_CHIP_SELECT_REQUIRED) {
+    pinMode(PIN_SPI_CHIP_SELECT_REQUIRED, OUTPUT);
+  }
+
+  if (!sd.begin(PIN_SD_CHIP_SELECT, SPI_QUARTER_SPEED)) {
+    sd.initErrorHalt();
+  }
+}
+
+/**
+ * Redoes GPS configuration, assuming it has factory-default settings.
+ *
+ * Although this should only have to be done once, the GPS module sometimes
+ * drops these customizations (possibly due to power brown-out).
+ */
+void resetGpsConfig() {
+  digitalWrite(PIN_STATUS_LED, HIGH);
+  nss.begin(DEFAULT_GPS_BAUD);
+  nss.println("$PMTK314,0,5,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0*28");
+  nss.println("$PMTK251,14400*29");
+  nss.flush();
+  nss.end();
+  digitalWrite(PIN_STATUS_LED, LOW);
+
+  nss.begin(GPS_BAUD);
+}
+
+void getFirstGpsSample() {
+  nss.begin(GPS_BAUD);
+
+  while (true) {
+    readFromGpsUntilSampleTime();
+    fillGpsSample(gps);
+    if (sample.fix_age_ms == TinyGPS::GPS_INVALID_AGE) {
+      Serial.println(F("Waiting for location fix."));
+    } else if (sample.fix_age_ms == TinyGPS::GPS_INVALID_AGE) {
+      Serial.println(F("Waiting for datetime fix."));
+    } else {
+      Serial.println(F("Got GPS fix."));
+      break;
+    }
+  }
+}
+
+static void readFromGpsUntilSampleTime() {
+  unsigned long start = millis();
+  // Process a sample from the GPS every second.
+  while (millis() - start < SAMPLE_INTERVAL_MS) {
+    readFromGpsSerial();
+    getVoltageMaybeExit();
+  }
+}
+
+static bool readFromGpsSerial() {
+  while (nss.available()) {
+    gps.encode(nss.read());
+  }
+}
+
+static void startFilesOnSdNoSync() {
+  // directory
+  sprintf(
+      buf,
+      "%02d%02d%02d",
+      sample.year,
+      sample.month,
+      sample.day);
+  if (!sd.exists(buf)) {
+    if (!sd.mkdir(buf)) {
+      sd.errorHalt("Creating log directory for today failed.");
+    }
+  }
+
+  // SdFat will silently die if given a filename longer than "8.3"
+  // (8 characters, a dot, and 3 file-extension characters).
+
+  // GPX log
+  openTimestampedFile(".gpx", gpxFile);
+  gpxFile.print(F(
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+    "<gpx version=\"1.0\">\n"
+    "\t<trk><trkseg>\n"));
+  gpxFile.print(F(GPX_EPILOGUE));
+
+  // input (battery) voltage log
+  openTimestampedFile("v.csv", voltageFile);
+  voltageFile.print(F("datetime,voltage\n"));
+}
+
+static void openTimestampedFile(const char *shortSuffix, SdFile &file) {
+  sprintf(
+      buf,
+      "%02d%02d%02d/%02d%02d%02d%s",
+      sample.year,
+      sample.month,
+      sample.day,
+      sample.hour,
+      sample.minute,
+      sample.second,
+      shortSuffix);
+  Serial.print(F("Starting file "));
+  Serial.println(buf);
+  if (sd.exists(buf)) {
+    Serial.println(F("warning: already exists, overwriting."));
+  }
+  if (!file.open(buf, O_CREAT | O_WRITE)) {
+    sd.errorHalt();
+  }
+}
+
+static void writeFloat(float v, SdFile &file, int precision) {
+  obufstream ob(buf, sizeof(buf));
+  ob << setprecision(precision) << v;
+  file.print(buf);
+}
+
+static void writeFormattedSampleDatetime(SdFile &file) {
+  sprintf(
+      buf,
+      "%04d-%02d-%02dT%02d:%02d:%02d.%03dZ",
+      sample.year,
+      sample.month,
+      sample.day,
+      sample.hour,
+      sample.minute,
+      sample.second,
+      sample.hundredths);
+  file.print(buf);
+}
+
+static void writeGpxSampleToSd() {
+  gpxFile.seekSet(gpxFile.fileSize() + SEEK_TRKPT_BACKWARDS);
+  gpxFile.print(F("\t\t<trkpt "));
+
+  gpxFile.print(F("lat=\""));
+  writeFloat(sample.lat_deg, gpxFile, LATLON_PREC);
+  gpxFile.print(F("\" lon=\""));
+  writeFloat(sample.lon_deg, gpxFile, LATLON_PREC);
+  gpxFile.print(F("\">"));
+  getVoltageMaybeExit();
+
+  gpxFile.print(F("<time>"));
+  writeFormattedSampleDatetime(gpxFile);
+  gpxFile.print(F("</time>"));
+
+  if (sample.altitude_m != TinyGPS::GPS_INVALID_F_ALTITUDE) {
+    gpxFile.print(F("<ele>")); // meters
+    writeFloat(sample.altitude_m, gpxFile, 2 /* centimeter precision */);
+    gpxFile.print(F("</ele>"));
+  }
+  getVoltageMaybeExit();
+
+  if (sample.speed_mps != TinyGPS::GPS_INVALID_F_SPEED) {
+    gpxFile.print(F("<speed>"));
+    writeFloat(sample.speed_mps, gpxFile, 1);
+    gpxFile.print(F("</speed>"));
+  }
+  if (sample.course_deg != TinyGPS::GPS_INVALID_F_ANGLE) {
+    gpxFile.print(F("<course>"));
+    writeFloat(sample.course_deg, gpxFile, 1);
+    gpxFile.print(F("</course>"));
+  }
+  getVoltageMaybeExit();
+
+  if (sample.satellites != TinyGPS::GPS_INVALID_SATELLITES) {
+    gpxFile.print(F("<sat>"));
+    gpxFile.print(sample.satellites);
+    gpxFile.print(F("</sat>"));
+  }
+  if (sample.hdop_hundredths != TinyGPS::GPS_INVALID_HDOP) {
+    gpxFile.print(F("<hdop>"));
+    writeFloat(sample.hdop_hundredths / 100.0, gpxFile, 2);
+    gpxFile.print(F("</hdop>"));
+  }
+  getVoltageMaybeExit();
+
+  gpxFile.print(F("</trkpt>\n"));
+
+  gpxFile.print(F(GPX_EPILOGUE));
+
+  getVoltageMaybeExit();
+  digitalWrite(PIN_STATUS_LED, HIGH);
+  if (!gpxFile.sync() || gpxFile.getWriteError()) {
+    Serial.println(F("SD sync/write error."));
+  }
+  digitalWrite(PIN_STATUS_LED, LOW);
+}
+
+static void fillGpsSample(TinyGPS &gps) {
+  gps.f_get_position(
+      &sample.lat_deg,
+      &sample.lon_deg,
+      &sample.fix_age_ms);
+  sample.altitude_m = gps.f_altitude();
+
+  sample.satellites = gps.satellites();
+  sample.hdop_hundredths = gps.hdop();
+
+  sample.course_deg = gps.f_course();
+  sample.speed_mps = gps.f_speed_mps();
+
+  gps.crack_datetime(
+      &sample.year,
+      &sample.month,
+      &sample.day,
+      &sample.hour,
+      &sample.minute,
+      &sample.second,
+      &sample.hundredths,
+      &sample.datetime_fix_age_ms);
+}
+
+static float getVoltageMaybeExit() {
+  // TODO: Do safety calculation in int to be faster?
+  /*float voltage =
+      analogRead(PIN_BATTERY_DIVIDED_VOLTAGE) / VOLTAGE_ANALOG_CONSTANT;
+  float dv = voltage - lastVoltage;
+  // Regulated (USB) supply voltage is approximately 0 on the battery vin,
+  // otherwise abort if vin is low or falling.
+  if (voltage > 2.0
+      && (voltage < CUTOFF_VOLTAGE || dv < CUTOFF_DV)) {
+    gpxFile.close();
+    voltageFile.print(F("x"));
+    voltageFile.close();
+    digitalWrite(PIN_STATUS_LED, HIGH);
+    Serial.print(F("Exiting, final voltage was "));
+    Serial.println(voltage);
+    Serial.flush();
+    exit(0);
+    // TODO: Resume in case of a false alarm.
+  }
+  lastVoltage = voltage;
+  return voltage;*/
+  return 1;
+}
+
+static bool recordVoltage() {
+  float voltage = getVoltageMaybeExit();
+  if (voltage != lastRecordedVoltage
+      || lastRecordedVoltageMinute != sample.minute) {
+    writeFormattedSampleDatetime(voltageFile);
+    voltageFile.print(F(","));
+    writeFloat(voltage, voltageFile, 2);
+    voltageFile.print(F("\n"));
+    lastRecordedVoltageMinute = sample.minute;
+    lastRecordedVoltage = voltage;
+
+    digitalWrite(PIN_STATUS_LED, HIGH);
+    if (!voltageFile.sync() || voltageFile.getWriteError()) {
+      Serial.println(F("SD sync/write error."));
+    }
+    digitalWrite(PIN_STATUS_LED, LOW);
+  }
+
+}
