2b2c99bd9403732143477ed3029e43a47b00ff17
Patrick Itten <info@patrickitten.ch>
2020-07-07 09:39:21 +0200

    rewrite based on mark ficketts code. added display to his code

--- gps-logger/gps-logger.ino
+++ gps-logger/gps-logger.ino
@@ -1,12 +1,26 @@
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
-#include <SSD1306Ascii.h>
-#include <SSD1306AsciiAvrI2c.h>
-#include <SPI.h>
-#include <SD.h>
+/**
+ * GPS-Logger by Patrick Itten (c)2020
+ * based on the code of Mark Fickett : https://github.com/markfickett/gpslogger
+ */
 
-#define LOG_FREQ 10000
-#define UPD_FREQ 5000
+#include <SoftwareSerial.h>
+#include <SdFat.h>
+#include <TinyGPS.h>
+#include <SSD1306AsciiAvrI2c.h>
+
+#define SAMPLE_INTERVAL_MS 1000
+
+#define PIN_STATUS_LED 13
+
+#define PIN_RX_FROM_GPS 2
+#define PIN_TX_TO_GPS 3
+#define PIN_SD_CHIP_SELECT 4
+#define PIN_SPI_CHIP_SELECT_REQUIRED 4
+// The SD library requires, for SPI communication:
+// 11 MOSI (master/Arduino output, slave/SD input)
+// 12 MISO (master input, slave output)
+// 13 CLK (clock)
+#define GPS_BAUD 9600
 
 // Display
 // 0X3C+SA0 - 0x3C or 0x3D
@@ -15,32 +29,54 @@
 #define RST_PIN -1
 SSD1306AsciiAvrI2c oled;
 
-// GPS
-#define GPSBaud 9600
-// The TinyGPS++ object
-TinyGPSPlus gps;
-// The serial connection to the GPS device
-SoftwareSerial ss(2, 3); // (RX,TX)
+// Seek to fileSize + this position before writing track points.
+#define SEEK_TRKPT_BACKWARDS -24
+#define GPX_EPILOGUE "\t</trkseg></trk>\n</gpx>\n"
+#define LATLON_PREC 6
 
-// SD
-// set up variables using the SD utility library functions:
-// Sd2Card card;
-// SdVolume volume;
-// SdFile root;
-#define chipSelect 4
-bool sdFailure = false;
-unsigned long logTime;
+TinyGPS gps;
+SoftwareSerial nss(PIN_RX_FROM_GPS, PIN_TX_TO_GPS);
 
-// Buffer
-// global buffer for datapoints
-char buffer[42]; // longest buffer seems to be: "<trkpt lat="047.575336" lon="009.368504">" : 41 (+1 for NULL terminator)
-// int bufferSize = 0;
+SdFat sd;
+SdFile gpxFile;
 
-// GPX
-// #define GPX_HEADER ""
+// General-purpose text buffer used in formatting.
+char buf[32];
 
-void setup()
-{
+
+struct GpsSample {
+  float lat_deg,
+        lon_deg;
+  float altitude_m;
+
+  int satellites;
+  int hdop_hundredths;
+
+  // How many ms, according to millis(), since the last position data was read
+  // from the GPS.
+  unsigned long fix_age_ms;
+
+  float speed_mps;
+  float speed_kmh;
+  float speed_kts;
+  float course_deg;
+
+  // How many ms, according to millis(), since the last datetime data was read
+  // from the GPS.
+  unsigned long datetime_fix_age_ms;
+
+  int year;
+  byte month,
+       day,
+       hour,
+       minute,
+       second,
+       hundredths;
+};
+// The latest sample read from the GPS.
+struct GpsSample sample;
+
+void setup() {
   // DISPLAY
   if (RST_PIN >= 0){
     oled.begin(&Adafruit128x64, DISP_ADDRESS, RST_PIN);
@@ -49,281 +85,280 @@ void setup()
   } // Call oled.setI2cClock(frequency) to change from the default frequency.
   oled.setFont(font5x7);
   oled.clear();
-  oled.println(F("GPS LOGGER"));
-  oled.println(F("(c)2020 Patrick Itten"));
-  oled.println();
-  oled.println(F("initializing..."));
+  oled.println(F("GPS LOGGER V2\n(c)2020 Patrick Itten\n\ninitializing..."));
 
-  // SERIAL MONITOR
+  pinMode(PIN_STATUS_LED, OUTPUT);
+  digitalWrite(PIN_STATUS_LED, HIGH);
   Serial.begin(115200);
   while (!Serial);  // wait for serial port to connect. Needed for native USB
-  Serial.println(F("GPS-LOGGER"));
-  Serial.println(F("(c)2020 by Patrick Itten"));
-  Serial.println();
+  setUpSd();
+  getFirstGpsSample();
+  startFilesOnSdNoSync();
+  delay(5000);
+  digitalWrite(PIN_STATUS_LED, LOW);
+  oled.clear();
+}
 
-  // SD
-  Serial.println(F("Initializing SD card..."));
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    Serial.println(F("Card failed, or not present"));
-    oled.println(F("...no card found."));
-    sdFailure = true;
-  } else {
-    Serial.println(F("card initialized."));
-    oled.println(F("...card initialized."));
+void loop() {
+  readFromGpsUntilSampleTime();
+  fillGpsSample(gps);
+  if (sample.fix_age_ms <= SAMPLE_INTERVAL_MS) {
+    // TODO: Write whenever there is new data (trust GPS is set at 1Hz).
+    writeGpxSampleToSd();
+    displayGpxSample();
   }
-  Serial.println();
-  delay(3000);
+}
+
+void setUpSd() {
+  if (PIN_SD_CHIP_SELECT != PIN_SPI_CHIP_SELECT_REQUIRED) {
+    pinMode(PIN_SPI_CHIP_SELECT_REQUIRED, OUTPUT);
+    oled.println(F("SD initialized"));
+  }
+
+  if (!sd.begin(PIN_SD_CHIP_SELECT, SPI_QUARTER_SPEED)) {
+    sd.initErrorHalt();
+  }
+}
+
+void getFirstGpsSample() {
+  nss.begin(GPS_BAUD);
+
+  while (true) {
+    readFromGpsUntilSampleTime();
+    fillGpsSample(gps);
+    if (sample.fix_age_ms == TinyGPS::GPS_INVALID_AGE) {
+      Serial.println(F("Waiting for location fix."));
+    } else if (sample.fix_age_ms == TinyGPS::GPS_INVALID_AGE) {
+      Serial.println(F("Waiting for datetime fix."));
+    } else {
+      Serial.println(F("Got GPS fix."));
+      oled.println(F("Got GPS fix."));
+      break;
+    }
+  }
+}
+
+static void readFromGpsUntilSampleTime() {
+  unsigned long start = millis();
+  // Process a sample from the GPS every second.
+  while (millis() - start < SAMPLE_INTERVAL_MS) {
+    readFromGpsSerial();
+  }
+}
+
+static bool readFromGpsSerial() {
+  while (nss.available()) {
+    gps.encode(nss.read());
+  }
+}
+
+static void startFilesOnSdNoSync() {
+  // directory
+  sprintf(
+      buf,
+      "%02d%02d%02d",
+      sample.year,
+      sample.month,
+      sample.day);
+  if (!sd.exists(buf)) {
+    if (!sd.mkdir(buf)) {
+      oled.println(F("ERR: dir not created."));
+      sd.errorHalt("Creating log directory for today failed.");
+    }
+  }
+
+  // SdFat will silently die if given a filename longer than "8.3"
+  // (8 characters, a dot, and 3 file-extension characters).
+
+  // GPX log
+  openTimestampedFile(".gpx", gpxFile);
+  gpxFile.print(F(
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+    "<gpx version=\"1.0\">\n"
+    "\t<trk><trkseg>\n"));
+  gpxFile.print(F(GPX_EPILOGUE));
+}
+
+static void openTimestampedFile(const char *shortSuffix, SdFile &file) {
+  sprintf(
+      buf,
+      "%02d%02d%02d/%02d%02d%02d%s",
+      sample.year,
+      sample.month,
+      sample.day,
+      sample.hour,
+      sample.minute,
+      sample.second,
+      shortSuffix);
+  Serial.print(F("Starting file "));
+  Serial.println(buf);
+  oled.print(F("f:"));
+  oled.println(buf);
+  if (sd.exists(buf)) {
+    Serial.println(F("warning: already exists, overwriting."));
+    oled.println(F("warning: already exists, overwriting."));
+  }
+  if (!file.open(buf, O_CREAT | O_WRITE)) {
+    sd.errorHalt();
+  }
+}
+
+static void writeFloat(float v, SdFile &file, int precision) {
+  obufstream ob(buf, sizeof(buf));
+  ob << setprecision(precision) << v;
+  file.print(buf);
+}
+
+static void writeFormattedSampleDatetime(SdFile &file) {
+  sprintf(
+      buf,
+      "%04d-%02d-%02dT%02d:%02d:%02d.%03dZ",
+      sample.year,
+      sample.month,
+      sample.day,
+      sample.hour,
+      sample.minute,
+      sample.second,
+      sample.hundredths);
+  file.print(buf);
+}
+
+static void writeGpxSampleToSd() {
+  gpxFile.seekSet(gpxFile.fileSize() + SEEK_TRKPT_BACKWARDS);
+  gpxFile.print(F("\t\t<trkpt "));
+
+  gpxFile.print(F("lat=\""));
+  writeFloat(sample.lat_deg, gpxFile, LATLON_PREC);
+  gpxFile.print(F("\" lon=\""));
+  writeFloat(sample.lon_deg, gpxFile, LATLON_PREC);
+  gpxFile.print(F("\">"));
+
+  gpxFile.print(F("<time>"));
+  writeFormattedSampleDatetime(gpxFile);
+  gpxFile.print(F("</time>"));
+
+  if (sample.altitude_m != TinyGPS::GPS_INVALID_F_ALTITUDE) {
+    gpxFile.print(F("<ele>")); // meters
+    writeFloat(sample.altitude_m, gpxFile, 2 /* centimeter precision */);
+    gpxFile.print(F("</ele>"));
+  }
+
+  if (sample.speed_mps != TinyGPS::GPS_INVALID_F_SPEED) {
+    gpxFile.print(F("<speed>"));
+    writeFloat(sample.speed_mps, gpxFile, 1);
+    gpxFile.print(F("</speed>"));
+  }
+  if (sample.course_deg != TinyGPS::GPS_INVALID_F_ANGLE) {
+    gpxFile.print(F("<course>"));
+    writeFloat(sample.course_deg, gpxFile, 1);
+    gpxFile.print(F("</course>"));
+  }
+
+  if (sample.satellites != TinyGPS::GPS_INVALID_SATELLITES) {
+    gpxFile.print(F("<sat>"));
+    gpxFile.print(sample.satellites);
+    gpxFile.print(F("</sat>"));
+  }
+  if (sample.hdop_hundredths != TinyGPS::GPS_INVALID_HDOP) {
+    gpxFile.print(F("<hdop>"));
+    writeFloat(sample.hdop_hundredths / 100.0, gpxFile, 2);
+    gpxFile.print(F("</hdop>"));
+  }
+
+  gpxFile.print(F("</trkpt>\n"));
+
+  gpxFile.print(F(GPX_EPILOGUE));
+
+  digitalWrite(PIN_STATUS_LED, HIGH);
+  if (!gpxFile.sync() || gpxFile.getWriteError()) {
+    Serial.println(F("SD sync/write error."));
+    oled.clear();
+    oled.println(F("Err: SD write error."));
+    delay(1000);
+  }
+  digitalWrite(PIN_STATUS_LED, LOW);
+}
+
+static void displayGpxSample() {
+
   oled.clear();
 
-  // GPS
-  ss.begin(GPSBaud);
-
-}
-
-void loop()
-{
-
-  if (gps.location.isUpdated()){
-    showInfo(true); // true: new position
-    if (millis() - logTime > LOG_FREQ ){
-      writeLog();
-      logTime = millis();
-    }
-  } else {
-    showInfo(false); // false: same position
-  }
-  smartDelay( UPD_FREQ ); // Update frequency
-
-  if (millis() > 5000 && gps.charsProcessed() < 10)
-    Serial.println(F("No GPS data received: check wiring"));
-}
-
-void showInfo(bool updated){
-
-  Serial.println();
-
-  if (updated){
-    oled.clear();
-  } else {
-    oled.clear(0,oled.displayWidth(), 0, 2);
-  }
-
-  // Date
-  loadBufferDate(gps.date, "day: ");
-  Serial.print(buffer);
-  oled.println(buffer);
+  //Date
+  sprintf(buf, "%04d-%02d-%02d", sample.year, sample.month, sample.day);
+  oled.println(buf);
 
   // Time
-  loadBufferTime(gps.time, "utc: ");
-  Serial.print(buffer);
-  oled.println(buffer);
+  sprintf(buf, "%02d:%02d:%02d.%03d", sample.hour, sample.minute, sample.second, sample.hundredths);
+  oled.println(buf);
 
   // Satellites
-  loadBuffer(gps.satellites.value(), gps.satellites.isValid(), "sat: ", "", 1, 0);
-  Serial.print(buffer);
-  oled.println(buffer);
+  addPrefixSuffix(sample.satellites, true, "sat: ", "", 1, 0);
+  oled.println(buf);
 
-  if (updated){
-    // HDOP
-    loadBuffer(gps.hdop.hdop(), gps.hdop.isValid(), "hdop: ", "", 3, 1);
-    Serial.print(buffer);
+  // Latitude
+  addPrefixSuffix(sample.lat_deg, true, "lat: ", "", 3, LATLON_PREC);
+  oled.println(buf);
 
-    // Latitude
-    loadBuffer(gps.location.lat(), gps.location.isValid(), "lat: ", "", 3, 5);
-    Serial.print(buffer);
-    oled.println(buffer);
+  // Longitude
+  addPrefixSuffix(sample.lon_deg, true, "lon: ", "", 3, LATLON_PREC);
+  oled.println(buf);
 
-    // Longitude
-    loadBuffer(gps.location.lng(), gps.location.isValid(), "lng: ", "", 3, 5);
-    Serial.print(buffer);
-    oled.println(buffer);
+  // Altitude
+  addPrefixSuffix(sample.altitude_m, (sample.altitude_m != TinyGPS::GPS_INVALID_F_ALTITUDE), "alt: ", "m", 3, 1);
+  oled.println(buf);
 
-    // Altitude
-    loadBuffer(gps.altitude.meters(), gps.altitude.isValid(), "alt: ", "m", 3, 1);
-    Serial.print(buffer);
-    oled.println(buffer);
+  // Speed
+  addPrefixSuffix(sample.speed_kmh, (sample.speed_kmh != TinyGPS::GPS_INVALID_F_SPEED), "spd: ", "kmh", 3, 1);
+  oled.print(buf);
+  addPrefixSuffix(sample.speed_kts, (sample.speed_kts != TinyGPS::GPS_INVALID_F_SPEED), "", "kt", 3, 1);
+  oled.println(buf);
 
-    // Speed
-    loadBuffer(gps.speed.kmph(), gps.speed.isValid(), "spd: ", "kmh", 3, 1);
-    Serial.print(buffer);
-    oled.print(buffer);
-    loadBuffer(gps.speed.knots(), gps.speed.isValid(), "", "kt", 3, 1);
-    Serial.print(buffer);
-    oled.println(buffer);
+  // Course
+  addPrefixSuffix(sample.course_deg, (sample.course_deg != TinyGPS::GPS_INVALID_F_ANGLE), "crs: ", "", 1, 0);
+  oled.print(buf);
+  (sample.course_deg != TinyGPS::GPS_INVALID_F_ANGLE) ? oled.println(TinyGPS::cardinal(sample.course_deg)) : oled.println(F("***"));
 
-    // Course
-    loadBuffer(gps.course.deg(), gps.course.isValid(), "crs: ", "", 1, 0);
-    Serial.print(buffer);
-    oled.print(buffer);
-    
-    // Course Cardinal
-    strcpy (buffer, gps.course.isValid() ? TinyGPSPlus::cardinal(gps.course.deg()) : "***");
-    strcat (buffer, " ");
-    Serial.print(buffer);
-    oled.print(buffer);
-    //printMaxBufferSize();
-    // Serial.print(gps.course.isValid() ? TinyGPSPlus::cardinal(gps.course.deg()) : "** ");
-
-    // Fix age
-    loadBuffer(gps.location.age(), gps.location.isValid(), "age: ", "ms", 3, 0);
-    Serial.print(buffer);
-
-    // printInt(gps.charsProcessed(), true, 6);
-    // printInt(gps.sentencesWithFix(), true, 10);
-    // printInt(gps.failedChecksum(), true, 9);
-  }
-  if (sdFailure){
-    oled.print("   *SDerr");
-  }
 }
 
-void writeLog(){
-  char filename[16];
-  sprintf(filename, "%02d%02d%02d.gpx", gps.date.year(), gps.date.month(), gps.date.day());
-  File dataFile = SD.open(filename, FILE_WRITE);
-  if (dataFile) {
-    // char datarow[128];
-    // sprintf(datarow, "<trkpt lat=\"%.6f\" lon=\"%.6f\"></trkpt>", gps.location.lat(), gps.location.lng());
-//    dataFile.println(buffer);
-    char buf[32];
-    // opening
-    strcpy (buffer, "<trkpt lat=\"");
-    dtostrf (gps.location.lat(), 0, 6, buf);
-    strcat (buffer, buf);
-    strcat (buffer, "\" lon=\"");
-    dtostrf (gps.location.lng(), 0, 6, buf);
-    strcat (buffer, buf);
-    strcat (buffer, "\">");
-    dataFile.println(buffer);
-    //printMaxBufferSize();
-    
-    // time
-    strcpy (buffer, "<time>");
-    sprintf(buf, "%02d-%02d-%02dT%02d:%02d:%02dZ", gps.date.year(), gps.date.month(), gps.date.day(), gps.time.hour(), gps.time.minute(), gps.time.second());
-    strcat (buffer, buf);
-    strcat (buffer, "</time>");
-    dataFile.println(buffer);
-    //printMaxBufferSize();
-    // elevation
-    gpxBuffer("ele", gps.altitude.meters(), 0);
-    dataFile.println(buffer);
-    // course
-    gpxBuffer("course", gps.course.deg(), 0);
-    dataFile.println(buffer);
-    // speed
-    gpxBuffer("speed", gps.speed.mps(), 1);
-    dataFile.println(buffer);
-    // sat
-    gpxBuffer("sat", gps.satellites.value(), 0);
-    dataFile.println(buffer);
-    // hdop
-    gpxBuffer("hdop", gps.hdop.hdop(), 1);
-    dataFile.println(buffer);
-    // closing
-    dataFile.println("</trkpt>");
+static void fillGpsSample(TinyGPS &gps) {
+  gps.f_get_position(
+      &sample.lat_deg,
+      &sample.lon_deg,
+      &sample.fix_age_ms);
+  sample.altitude_m = gps.f_altitude();
 
-    dataFile.close();
-    sdFailure = false;
-    Serial.print("log written");
-  }
-  else {
-    Serial.println(F("error opening logfile"));
-    sdFailure = true;
-  }
+  sample.satellites = gps.satellites();
+  sample.hdop_hundredths = gps.hdop();
+
+  sample.course_deg = gps.f_course();
+  sample.speed_mps = gps.f_speed_mps();
+  sample.speed_kmh = gps.f_speed_kmph();
+  sample.speed_kts = gps.f_speed_knots();
+
+  gps.crack_datetime(
+      &sample.year,
+      &sample.month,
+      &sample.day,
+      &sample.hour,
+      &sample.minute,
+      &sample.second,
+      &sample.hundredths,
+      &sample.datetime_fix_age_ms);
 }
 
-static void smartDelay(unsigned long ms)
+static void addPrefixSuffix(float val, bool valid, char prefix[], char suffix[], int len, int prec) // len: minimal length of number part, prec: precision of number part
 {
-  // This custom version of delay() ensures that the gps object is being "fed".
-  unsigned long start = millis();
-  do 
-  {
-    while (ss.available())
-      gps.encode(ss.read());
-  } while (millis() - start < ms);
-}
-
-static void loadBuffer(float val, bool valid, char prefix[], char suffix[], int len, int prec) // len: minimal length of number part, prec: precision of number part
-{
-  strcpy (buffer, prefix);
+  strcpy (buf, prefix);
 
   if (!valid) {
     while (len-- > 0)
-      strcat (buffer, "*");
+      strcat (buf, "*");
   } else {
-    char buf[32];
-    dtostrf (val, len, prec, buf);
-    strcat (buffer, buf);
-    strcat (buffer, suffix);
+    char buffer[32];
+    dtostrf (val, len, prec, buffer);
+    strcat (buf, buffer);
+    strcat (buf, suffix);
   }
 
-  strcat (buffer, " ");
-  //printMaxBufferSize();
-  smartDelay(0);
+  strcat (buf, " ");
 }
-
-static void gpxBuffer(char tag[], float val, int prec)
-{
-  //open
-  strcpy (buffer, "<");
-  strcat (buffer, tag);
-  strcat (buffer, ">");
-
-  char buf[32];
-  dtostrf (val, 0, prec, buf);
-  strcat (buffer, buf);
-
-  //close
-  strcat (buffer, "</");
-  strcat (buffer, tag);
-  strcat (buffer, ">");
-  
-  // printMaxBufferSize();
-}
-
-static void loadBufferDate(TinyGPSDate &d, char prefix[])
-{
-  strcpy (buffer, prefix);
-  if (!d.isValid())
-  {
-    strcat(buffer, "********** ");
-  }
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d-%02d-%02d ", d.month(), d.day(), d.year());
-    strcat(buffer, sz);
-  }
-  //printMaxBufferSize();
-}
-
-static void loadBufferTime(TinyGPSTime &t, char prefix[])
-{
-  strcpy (buffer, prefix);
-  if (!t.isValid())
-  {
-    strcat(buffer, "******** ");
-  }
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
-    strcat(buffer, sz);
-  }
-  // printInt(d.age(), d.isValid(), 5);
-  smartDelay(0);
-}
-
-// static void printMaxBufferSize(){
-//   if (strlen(buffer) > bufferSize){
-//     Serial.println(F("********************"));
-//     Serial.print("length of \"");
-//     Serial.print(buffer);
-//     Serial.print("\" : ");
-//     Serial.println(strlen(buffer));
-//     Serial.println(F("********************"));
-
-//     bufferSize = strlen(buffer);
-//   }
-// }
